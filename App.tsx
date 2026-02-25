714cd651dca39c02731d2c6107d4f09de49d1537
stcy2828 <stcy2828@gmail.com>
2026-02-25 15:51:32 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
@@ -16,6 +15,17 @@ const App: React.FC = () => {
   const location = useLocation();
 
   useEffect(() => {
+    // 1. 處理舊網站殘留的 #news 錨點問題
+    // 如果網址帶有 #news，立即將其移除而不觸發頁面刷新
+    if (window.location.hash === '#news') {
+      window.history.replaceState(
+        null, 
+        document.title, 
+        window.location.pathname + window.location.search
+      );
+    }
+
+    // 2. 確保每次路由切換時，頁面都會自動捲動回最上方
     window.scrollTo(0, 0);
   }, [location]);
 
@@ -32,6 +42,8 @@ const App: React.FC = () => {
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/admin-login" element={<AdminLoginPage />} />
             <Route path="/admin/dashboard" element={<BackendMain />} />
+            {/* 如果訪客輸入了不存在的網址，自動導回首頁 */}
+            <Route path="*" element={<TopPage />} />
           </Routes>
         </div>
         <Footer />
